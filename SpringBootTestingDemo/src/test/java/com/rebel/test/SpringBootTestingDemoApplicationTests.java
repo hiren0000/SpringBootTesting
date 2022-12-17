532bdac93f4492c6b3434fd6baa1600a658b7798
Hiren <rebeldevmurari@gmail.com>
2022-12-17 13:25:25 +0000

    Initial commit

---
+++ SpringBootTestingDemo/src/test/java/com/rebel/test/SpringBootTestingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
